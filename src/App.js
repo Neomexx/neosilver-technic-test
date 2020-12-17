c3784cfcc810a5949fa7c1f2380288a9f885f6d4
axel.terrand <axel.terrand@epitech.eu>
2020-12-17 19:20:24 +0100

    [UPDATE] restart ok, game ok, bot button crash, win condition ok, GUI ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import BlueButton from "./Components/Button/BlueButton"
+import RestartButton from "./Components/Button/ButtonRestart"
+import BotButton from "./Components/Button/ButtonRestart"
 import React, { Component } from 'react';
-import RedButton from "./Components/Button/RedButton"
 import GridGame from "./Components/Grid/Grid"
-import { Button, Icon,  Grid, GridColumn } from 'semantic-ui-react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import { Layout } from 'antd';
@@ -11,35 +10,64 @@ const { Header, Footer, Content } = Layout;
 
  
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score1: 0,
+      score2: 0,
+      win: 0,
+      lose: 0,
+      bot: false,
+    }
+  }
+
+  changeCurrentPlayerState = (scorePlayer1, scorePlayer2) => {
+    this.setState({
+      score1: scorePlayer1,
+      score2: scorePlayer2,
+    })
+  }
+
+  changeWinState = (winPlayer, losePlayer) => {
+    this.setState({
+      win: winPlayer,
+      lose: losePlayer,
+    })
+  }
+
+  changeBotState = (bool) => {
+    this.setState({
+      bot: bool
+    })
+  }
+
   render() { 
     return (
-      
         <Layout>
           <Header>
             <h1 style={{
               color: 'white',
-              marginLeft: '4O%',
+              marginLeft: '40%',
             }}>Rock Paper Scissors</h1>
           </Header>
           <Content>
             <div style={{
-              minHeight: 500,
               padding: 24,
               background: 'white',
             }}>
-              <GridGame style={{
-
-              }}/>
+              <GridGame scorePlayer1={this.state.score1} scorePlayer2={this.state.score2} 
+                        win={this.state.win} lose={this.state.lose} bot={this.state.bot} updateScore={this.changeCurrentPlayerState} updateWinState={this.changeWinState}/>
             </div>
           </Content>
-        <Footer>
+        <Footer style={{background: '#F5F5F5', marginLeft:'42%', top: '30%'}}>
           <div>
-            <BlueButton label="start"/>
-            <RedButton label="stop"/>
+            {this.state.win === 1 && <label>You win</label>}
+            <BotButton label="bot" updateBot={this.changeBotState}/>
+            <RestartButton label="restart"  updateScore={this.changeCurrentPlayerState} updateWinState={this.changeWinState}/>
+            {this.state.lose === 1 && <label>You lose</label>}
           </div>
         </Footer>
         </Layout>
-      
     );
   }
 }
--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -1,67 +1,105 @@
 import {React, Component} from 'react';
-import { Button, Icon,  Grid, GridColumn } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css'
-import { Row, Col } from 'antd';
+import { Row, Col, Steps } from 'antd';
 
-/*class GridGame extends Component {
-    render() {
-      return (
-        <Grid columns={2}>
-            <Grid.Row>
-              <Grid.Column width={1}>
-                <Button icon>
-                  Stone 
-                  <Icon name='hand rock outline' />
-                </Button>
-              </Grid.Column>
-              <Grid.Column width={1}>
-                <Button icon>
-                <Icon name='hand scissors outline' />
-                  scissors
-                </Button>
-              </Grid.Column>
-              <Grid.Column width={1}>
-                <Button icon>
-                  <Icon name='hand paper outline' />
-                   paper
-                </Button>
-              </Grid.Column>
-            </Grid.Row>
+const styleLeft = { background: '#F5F5F5', padding: 50, 
+                    marginRight: '5%', marginLeft:'20%',
+                    };
+const styleRight = { background: '#F5F5F5', padding: 50};
+const styleButton = {padding: 25};
 
-            <Grid.Row>
-              <Grid.Column width={1}>
-                <Button icon>
-                  Stone 
-                  <Icon name='hand rock outline' />
-                </Button>
-              </Grid.Column>
-              <Grid.Column width={1}>
-                <Button icon>
-                <Icon name='hand scissors outline' />
-                  scissors
-                </Button>
-              </Grid.Column>
-              <Grid.Column width={1}>
-                <Button icon>
-                  <Icon name='hand paper outline' />
-                   paper
-                </Button>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-      );
-    }
-  }*/
-
-const style = { background: '#DCDCDC', padding: 50, marginRight: 10   };
+const { Step } = Steps;
 
 class GridGame extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        botMove: 0,
+      }
+    }
+
+    getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    HandleGamePoints(scorePlayer1, scorePlayer2) {
+      if (scorePlayer1 === 3) {
+        this.props.updateWinState(1, 0);
+      }
+      else if (scorePlayer2 === 3) {
+        this.props.updateWinState(0, 1);
+      }
+    }
+
+    gameChoice(choice) {
+      this.botMove = this.getRandomInt(3);
+
+      console.log("props score 1 " + this.props.scorePlayer1)
+      console.log("props score 2 " + this.props.scorePlayer2)
+      
+      if ((this.botMove === 0 && choice === "paper") ||
+          (this.botMove === 1 && choice === "scissors") ||
+          (this.botMove === 2 && choice === "rock")) {
+            this.props.updateScore(this.props.scorePlayer1 + 1, this.props.scorePlayer2)
+      }
+      else if ((this.botMove === 0 && choice === "scissors") || 
+              (this.botMove === 1 && choice === "rock") ||
+              (this.botMove === 2 && choice === "paper")) {
+                this.props.updateScore(this.props.scorePlayer1, this.props.scorePlayer2 + 1)               
+      }
+      this.HandleGamePoints(this.props.scorePlayer1, this.props.scorePlayer2);
+    }
+
+    BotChoices() {
+      
+    }
+
     render() {
         return (
         <Row>
-            <Col className="gutter-row" span={11} style={style}>col
+          <Col style={{position: 'absolute', left: '1%'}}>
+          <Steps direction="vertical" current={this.props.scorePlayer1}>
+            <Step title="point" />
+            <Step title="points" />
+            <Step title="points" />
+          </Steps>
+          </Col>
+          <Col className="gutter-row" span={6} style={styleLeft}>
+          { this.props.bot === false &&<Button icon style={styleButton} onClick={() => this.gameChoice("rock")}>
+              Stone 
+              <Icon name='hand rock outline' />
+            </Button> }
+            <Button icon style={styleButton} onClick={() => this.gameChoice("paper")}>
+              <Icon name='hand paper outline' />
+              paper
+            </Button>
+            <Button icon style={styleButton} onClick={() => this.gameChoice("scissors")}>
+              <Icon name='hand scissors outline' />
+                scissors
+            </Button>
             </Col>
-            <Col span={11} style={style}>col</Col>
+            <Col span={6} style={styleRight}>
+            <Button icon style={styleButton}>
+              Stone 
+              <Icon name='hand rock outline' />
+            </Button>
+            <Button icon style={styleButton}>
+              <Icon name='hand paper outline' />
+              paper
+            </Button>
+            <Button icon style={styleButton}>
+              <Icon name='hand scissors outline' />
+                scissors
+            </Button>
+          </Col>
+          <Col style={{position: 'absolute', right: '1%'}}>
+          <Steps direction="vertical" current={this.props.scorePlayer2}>
+            <Step title="point" />
+            <Step title="points" />
+            <Step title="points" />
+          </Steps>
+          </Col>
         </Row>
         );
     }
